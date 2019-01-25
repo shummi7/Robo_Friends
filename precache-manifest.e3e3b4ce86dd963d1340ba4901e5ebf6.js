195ced0c7d486d86c6370bbcc9a30cc6f738d27b
shummi7 <sharmila16moorthi@gmail.com>
2019-01-25 13:53:08 -0800

    Updates

---
+++ precache-manifest.e3e3b4ce86dd963d1340ba4901e5ebf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo_Friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bb14eb816c2b660dbde8",
+    "url": "/Robo_Friends/static/js/runtime~main.bb14eb81.js"
+  },
+  {
+    "revision": "0750ef86e37d0f5e37d2",
+    "url": "/Robo_Friends/static/js/main.0750ef86.chunk.js"
+  },
+  {
+    "revision": "917269d29b6fa45804c3",
+    "url": "/Robo_Friends/static/js/1.917269d2.chunk.js"
+  },
+  {
+    "revision": "0750ef86e37d0f5e37d2",
+    "url": "/Robo_Friends/static/css/main.6bbdf707.chunk.css"
+  },
+  {
+    "revision": "917269d29b6fa45804c3",
+    "url": "/Robo_Friends/static/css/1.8c4c34be.chunk.css"
+  },
+  {
+    "revision": "aeca91da30bda08972c8163d3e445e35",
+    "url": "/Robo_Friends/index.html"
+  }
+];
