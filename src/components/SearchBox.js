3b81e89416ec59db867c88d953e097b24ffac82d
shummi7 <sharmila16moorthi@gmail.com>
2019-01-31 18:54:12 -0800

    revert changes

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,9 +6,9 @@ const SearchBox = ({ searchfield, searchChange }) => {
       <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
-        placeholder='Search Robots' 
+        placeholder='search robots'
         onChange={searchChange}
-      /> 
+      />
     </div>
   );
 }
